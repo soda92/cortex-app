69fac7f90bfa3d002b589bd6453e7629a76e86b0
SodaCris <wang-jiaxun@outlook.com>
2022-05-11 23:01:42 +0800

    add scripts

---
+++ run.ps1
@@ -0,0 +1,6 @@
+qemu-system-arm `
+  -cpu cortex-m3 `
+  -machine lm3s6965evb `
+  -nographic `
+  -semihosting-config enable=on,target=native `
+  -kernel target/thumbv7m-none-eabi/debug/examples/hello
